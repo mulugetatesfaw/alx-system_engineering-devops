a732a02e25660d029272ef7c3d0095d1bec47320
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-04-21 12:34:41 -0700

    package install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# This Puppet manifest installs puppet-lint
+# install flask -v 2.1.0
 
-package { 'flask:
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'flask':
+  command => '/usr/bin/apt-get -pip3 install puppet-lint -v 2.1.0',
 }
